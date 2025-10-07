62fe47be7baf0b31ba8863cdc3bf235b0ccb7720
Bat-Amgalan369 <fn.dvlpr@gmail.com>
2025-10-07 22:31:16 +0800

    Initial commit on amgalan branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutBeg from "./about/one";
 import Erxes from "./Erxes-Academy/page";
 
 export default function Home() {
   return (
     <div>
       <Erxes />
+      <AboutBeg/>
     </div>
   );
 }
